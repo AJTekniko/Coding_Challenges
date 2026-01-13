5c189c33495c50645ede7f7056ee51e261e2572f
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-01-13 00:19:22 -0700

    Create 20260112 Contenido Pregunta.py

---
+++ 20260112 Contenido/20260112 Contenido Pregunta.py
@@ -0,0 +1,13 @@
+"""
+Video: https://www.youtube.com/shorts/W_KM8kXncOw
+
+Pregunta de entrevista:
+
+¿Usted puede crear un programa que toma a una matriz de objetos, que en este caso representa a los osos de un bosque con el nombre y el nivel de hambre que tiene cada oso, y que retorna el nombre de los osos que están más habrientos que el promedio en orden alfabético? ¿Cuál sería la menor cantidad de líneas que ocuparía su programa?
+
+Por ejemplo, el programa debería de retornar los nombres que ve en pantalla con esa matriz.
+
+Próximamente se subirá un video con un ejemplo para esta pregunta.
+
+Fuente de la pregunta: Cassidy Williams (Cassidoo)
+"""
