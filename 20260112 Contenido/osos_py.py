3a9e2a8581efc8d9f6ed6bf1621cdfef5c420a1e
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-01-14 01:19:09 -0700

    Create osos_py.py

---
+++ 20260112 Contenido/osos_py.py
@@ -0,0 +1,7 @@
+osos = [
+  { "nombre": 'Baloo', "hambre": 6 },
+  { "nombre": 'Yogi', "hambre": 9 },
+  { "nombre": 'Paddington', "hambre": 4 },
+  { "nombre": 'Winnie', "hambre": 10 },
+  { "nombre": 'Chicago', "hambre": 20 },
+]
