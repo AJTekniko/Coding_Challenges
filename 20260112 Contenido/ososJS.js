d0016fcd976b135cd75af5f66e34aad72cbb2f8d
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-01-16 20:56:35 -0700

    Add files via upload

---
+++ 20260112 Contenido/ososJS.js
@@ -0,0 +1,7 @@
+export let osos = [
+  { nombre: 'Baloo', hambre: 6 },
+  { nombre: 'Yogi', hambre: 9 },
+  { nombre: 'Paddington', hambre: 4 },
+  { nombre: 'Winnie', hambre: 10 },
+  { nombre: 'Chicago', hambre: 20 },
+];
