c371050296eeb0756ceaabd1e84e73cfa4298608
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-01-19 20:31:55 -0700

    Create 20260119 Contenido Pregunta.py

---
+++ 20260119 Contenido/20260119 Contenido Pregunta.py
@@ -0,0 +1,11 @@
+"""
+Video: https://www.youtube.com/shorts/wHm1Ylch_b4
+
+Pregunta de entrevista:
+
+¿Usted puede crear un programa que toma a una cadena de caracteres y que retorna una subcadena contigua con una longitud de 10 y con caracteres que pueden ser mapeados de manera biyectiva a los movimientos arriba, abajo, izquierda, derecha, B, A representados por sus siglas en inglés `{U,D,L,R,B,A}` para que la subcadena se decodifique al código Konami `"UUDDLRLRBA"`? En este caso, un carácter siempre se mapea al mismo movimiento y dos movimientos diferentes no pueden compartir carácter. Por favor escriba un programa que retorna un mapeo válido en la forma de un objeto como los ejemplos que ve en pantalla.
+
+Próximamente se subirá un video con un ejemplo para esta pregunta.
+
+Fuente de la pregunta: Cassidy Williams (Cassidoo)
+"""
