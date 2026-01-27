1eda5c758867f5424bb5a86177d2342129fa09b2
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-01-27 00:08:42 -0700

    Create 20260126 Contenido Pregunta.py

---
+++ 20260126 Contenido/20260126 Contenido Pregunta.py
@@ -0,0 +1,11 @@
+"""
+Video: https://www.youtube.com/shorts/NysB-ckYhyk
+
+Pregunta de Entrevista:
+
+¿Usted puede crear un programa que analiza una cadena de caracteres en minúsculas con un espacio entre cada palabra para determinar cuántas vocales existen en la primer palabra, y que invierte las palabras sucesivas si tienen la misma cantidad de vocales que la primer palabra? Por favor escriba un programa que produce el ejemplo que ve en pantalla.
+
+Próximamente se subirá un video con un ejemplo para esta pregunta.
+
+Fuente de la pregunta: Cassidy Williams (Cassidoo)
+"""
