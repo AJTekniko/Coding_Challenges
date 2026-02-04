34b67181b1128f1889982724c662c93b8faa777a
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-02-03 20:12:42 -0700

    Create interview question for perfect months
    Add interview question about perfect months in Gregorian calendar.

---
+++ 20260202 Contenido/20260202 Contenido Pregunta.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/MENx3BXwc1k
+
+Pregunta de entrevista:
+
+Febrero de 2026 es un mes perfecto. Para que un mes sea considerado perfecto, necesita empezar en el primer día de la semana, el cual ocurre en domingo o lunes dependiendo en el calendario, y necesita ser divisible por siete. ¿Usted puede crear una función que retorna el mes perfecto más reciente y el siguiente mes perfecto para el año Gregoriano proporcionado? Por favor escriba una función que produce los ejemplos que ve en pantalla.
+
+Próximamente se subirá un video con un ejemplo para esta pregunta.
+
+Fuente de la pregunta: Cassidy Williams (Cassidoo)
+"""
