ca8c1737a7116dbbc8895abcedeae99d0f3e9898
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-02-09 23:52:39 -0700

    Add programming challenge description in Python script
    Added a Python script that describes a programming challenge involving moving identical numbers in an array to the end.

---
+++ 20260209 Contenido/20260209 Contenido Pregunta.py
@@ -0,0 +1,9 @@
+"""
+Video: youtube.com/shorts/4UmBOqqjgPM
+
+¿Usted puede crear un programa que toma una matriz de números enteros y un número proporcionado, y que mueve todos los números de la matriz idénticos al número proporcionado hasta el final sin afectar el orden relativo de los demás números? Por favor escriba un programa que produce el ejemplo que ve en pantalla sin crear una copia de la matriz.
+
+Próximamente se subirá un video con un ejemplo para esta pregunta.
+
+Fuente de la pregunta: Cassidy Williams (Cassidoo)
+"""
