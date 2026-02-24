efd577c95c94934d005da0955ded69de9f2ea2da
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-02-23 23:46:28 -0700

    Create 20260223 Contenido Pregunta Py.py

---
+++ 20260223 Contenido/20260223 Contenido Pregunta Py.py
@@ -0,0 +1,9 @@
+"""
+Video: youtube.com/shorts/hc0mPExb1Ds
+
+Si a usted se le proporciona una matriz de números enteros, ¿puede encontrar la submatriz contigua que contiene la suma más grande y retornar dicha suma? La submatriz debe contener por lo menos un elemento. Si todos los elementos son negativos, por favor retorne la submatriz que contiene el mayor valor, o sea la menos negativa. Considere los ejemplos que ve en pantalla. Si necesita una pista para crear el código, busque el Algoritmo de Kadane.
+
+Próximamente se subirá un video con un ejemplo para esta pregunta.
+
+Fuente de la pregunta: Cassidy Williams (Cassidoo)
+"""
