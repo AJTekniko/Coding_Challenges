8ab674102bde11bf3058565386630a49922dce40
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-02 22:55:21 -0700

    Create 20260302 Contenido Pregunta Py.py

---
+++ 20260302 Contenido/20260302 Contenido Pregunta Py.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/8I8StA2nwOA
+
+Pregunta de entrevista:
+
+¿Puede encontrar el elemento mayoritario de una matriz (aquel que aparece más que n/2 veces) en tiempo de orden lineal O(n) y espacio de orden constante O(1) sin utilizar mapas hash? Considere los ejemplos que ve en pantalla. Si necesita una pista para crear el código, busque el Algoritmo de Voto Mayoritario de Boyer-Moore.
+
+Próximamente se subirá un video con un ejemplo para esta pregunta.
+
+Fuente de la pregunta: Cassidy Williams (Cassidoo)
+"""
