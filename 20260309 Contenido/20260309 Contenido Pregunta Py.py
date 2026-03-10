1d846131d75eca1a2a8cde9b9b7dd19a33727857
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-03-09 23:07:04 -0600

    Create 20260309 Contenido Pregunta Py.py

---
+++ 20260309 Contenido/20260309 Contenido Pregunta Py.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/ASAfjXkqBRk
+
+Pregunta de entrevista:
+
+Si a usted se le proporciona una cadena de caracteres s que consiste de puras a's y b's, y si se le permite intercambiar caracteres adjuntos cualquier número de veces, ¿puede retornar la cantidad mínima de cambios necesarios para transformar s en una cadena alterna o -1 si acaso es imposible? Considere los ejemplos que ve en pantalla. Por favor escriba un programa que retorne esos resultados.
+
+Próximamente se subirá un video con un ejemplo para esta pregunta.
+
+Fuente de la pregunta: Cassidy Williams (Cassidoo)
+"""
