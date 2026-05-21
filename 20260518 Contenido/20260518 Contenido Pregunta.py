a5fe7b0145f39993829deb71ef3a84e8d9f50c47
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-20 23:54:04 -0600

    Create 20260518 Contenido Pregunta.py

---
+++ 20260518 Contenido/20260518 Contenido Pregunta.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/ZtA7scXNH6g
+
+Pregunta de entrevista:
+
+Si a usted se le proporciona una cadena de caracteres s que consiste de letras, ¿puede cambiar cada letra minúscula a mayúscula y vice versa? Y como bono, ¿puede agregar un parámetro de alternación que resulta en TeXtO aLtErNaDo? Considere los ejemplos que ve en pantalla. Por favor escriba un programa que retorne esos resultados.
+
+Próximamente se subirá un video con un ejemplo para esta pregunta.
+
+Fuente de la pregunta: Cassidy Williams (Cassidoo)
+"""
