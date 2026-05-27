700b41ccfd33e9edd5a8e4707ca06be7c82761db
AJTekniko <240049008+AJTekniko@users.noreply.github.com>
2026-05-26 23:18:45 -0600

    Create 20260525 Contenido Pregunta.py

---
+++ 20260525 Contenido/20260525 Contenido Pregunta.py
@@ -0,0 +1,11 @@
+"""
+Video: youtube.com/shorts/sY_dz8MXeG4
+
+Pregunta de entrevista:
+
+Si a usted se le proporciona una cola de clientes y un número entero n, ¿puede mover cada enésimo cliente al final de la línea y mantener el orden relativo? Considere los ejemplos que ve en pantalla. Por favor escriba un programa que retorne esos resultados.
+
+Próximamente se subirá un video con un ejemplo para esta pregunta.
+
+Fuente de la pregunta: Cassidy Williams (Cassidoo)
+"""
